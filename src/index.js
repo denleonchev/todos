f335c507ced4c00f0190f8bf0b39ef8ecb997497
denleonchev <den.leonchev@gmail.com>
2017-10-24 09:51:07 +0300

    Lesson 4 is in progress

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import App from './components/App';
+import { saveState, loadState } from './localStorage';
 
-const store = createStore(todoApp);
+const store = createStore(todoApp, loadState());
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+}, 1000))
 
 render(
   <Provider store={store}>
